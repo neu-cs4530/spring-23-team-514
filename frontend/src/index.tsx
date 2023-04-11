87d197de211deea9a99469173fbe7ceb4fb466eb
Candice Mac <65557180+cmac4396@users.noreply.github.com>
2023-04-11 16:52:29 -0400

    Toast duplication solved. Turned off React Strict Mode.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root'),
+  <App />,
   document.getElementById('root'),
 );
 
